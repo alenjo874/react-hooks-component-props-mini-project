5a8f0195d634086bfad6a6cb979c86ed20099cf9
Alen Jo <alenjo874@gmail.com>
2022-01-31 15:57:06 -0500

    completed assignemnt

---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  const article = posts.map((post) => {
+    return (
+      <Article title={post.title} date={post.date} preview={post.preview} />
+    );
+  });
+
+  return <main>{article}</main>;
+}
+
+export default ArticleList;
